676bf237946e78064951efc341acead7312d434f
kosukecoulomb <jobch.coulomb@gmail.com>
2022-07-28 17:02:36 +0900

    [firstcommit]Java基礎学習

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args)  {
+	String word;
+	word = "Kosuke";	
+	System.out.println(word);
+	
+	word = "SnowBoard";
+	System.out.println(word);
+	
+	final int LUCKEY_NUMBER = 7;
+	System.out.println(LUCKEY_NUMBER);
+	}
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,27 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 55555555L;
+		System.out.println(long1);
+		
+		double double1 = 0.5555;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		
+		Float f = 1.32F;
+		System.out.println(f);
+		
+		Integer i = 5;
+		System.out.println(i);
+		
+		Character c = 'C';
+		System.out.println(c);
+		
+		
+		int[] array = { 0, 1, 2, 3, 4 };
+		System.out.println(array[3]);
+		}
+	}
