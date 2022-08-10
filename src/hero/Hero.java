10b840fc05e59c8a718e9b522d9d3798557a155c
kosukecoulomb <jobch.coulomb@gmail.com>
2022-08-11 01:32:21 +0900

    [learn]クラス

--- src/hero/Hero.java
+++ src/hero/Hero.java
@@ -3,7 +3,7 @@ package hero;
 public class Hero {
 	String name;
 	int hp;
-	
+
 	public void sleep() {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した");
--- src/hero/Matango.java
+++ src/hero/Matango.java
@@ -3,4 +3,8 @@ package hero;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;
+	char suffix;
+	public void run() {
+		System.out.println("おばけキノコ" + this.suffix + "は逃げ出した!");
+	}
 }
