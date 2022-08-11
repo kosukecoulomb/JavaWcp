cccdbc1787be129e80b2a9454890c7d16e5edcdb
kosukecoulomb <jobch.coulomb@gmail.com>
2022-08-11 12:09:01 +0900

    [learn]コンストラクタ

--- src/hero/Hero.java
+++ src/hero/Hero.java
@@ -3,6 +3,12 @@ package hero;
 public class Hero {
 	String name;
 	int hp;
+	//Sword sword;
+
+	public void attack() {
+		//System.out.println(this.name + "は" + this.sword.name + "で攻撃した");
+		System.out.println("敵に5ポイントのダメージを与えた");
+	}
 
 	public void sleep() {
 		this.hp = 100;
@@ -23,4 +29,5 @@ public class Hero {
 		System.out.println("GAMEOVER");
 		System.out.println("最終HPは、" + this.hp + "でした");
 	}
+
 }
--- src/hero/Main.java
+++ src/hero/Main.java
@@ -1,15 +1,9 @@
 package hero;
 
 public class Main {
- public static void main(String[] arg) {
-	 
-	 Hero h = new Hero();
-	 h.name = "ミナト";
-	 h.hp = 100;
-	 System.out.println("勇者" + h.name + "を生み出しました!");
-	 h.sit(5);
-	 h.slip();
-	 h.sit(25);
-	 h.run();
+ public static void main(String[] args) {
+	 SuperHero sh = new SuperHero();
+	 sh.name = "test";
+	 sh.run();
  }
 }
